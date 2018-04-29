52baf6d389b600a24f53b2f56a082dc762a6ab80
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 09:32:26 +0200

    Update test.js

--- commands/Umaru/test.js
+++ commands/Umaru/test.js
@@ -1,6 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client ();
+
 function test(message,prefix,client){
   if (message.content.startsWith(prefix + "test")) {
-     message.channel.send("J'ai assez travaillé Oni-chan laisse moi jouer https://giphy.com/gifs/christmas-meme-uNf5zeBaofZNC");
+                const embed = new Discord.RichEmbed()
+            .setColor(0x00ffff)
+            .setDescription("J'ai assez travaillé Oni-chan laisse moi jouer")
+            .setImage("https://giphy.com/gifs/christmas-meme-uNf5zeBaofZNC")
+          
+            message.channel.send({embed}); 
+    
     }
 }
     module.exports = test
