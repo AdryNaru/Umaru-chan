eea9dede5cf715d031454e486837e8363842bf95
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-05-02 11:26:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const token = process.env.TOKEN;
-var prefix = "u!";
+const config = require("./config.json");
+const fs = require("fs");
+const token = process.env.TOKEN
 
 client.on("ready", () => {
 
 	var memberCount = client.users.size;
 	var serverCount = client.guilds.size;
-		client.user.setGame("fait u!help pour mes commandes Oni-chan");
-		client.user.setStatus("online");
-	console.log("--------------------------------------");
+		client.user.setGame("la console avec Kirie");
+		client.user.setStatus("idle");
+	console.log("Prete au travaille maître");
 	console.log("Utilisateurs: " + memberCount + "\nServeurs: " + serverCount);
 });
 
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    console.log
+    files.forEach(file => {
+        console.log
+        let eventFunction = require(`./commands/${file}`);
+        console.log
+        let eventName = file.split(".")[0];
+        console.log
+        client.on(eventName, (...args) => eventFunction.run(client, ...args));
+        console.log
+    });
+});
+
+client.on("message", (message) => {
+    console.log
+    if (message.author.bot) return;
+    console.log
+    if (message.content.indexOf(config.prefix) !==0) return;
+    console.log
+    
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try { 
+        console.log
+        let commandFile = require(`./commands/${command}.js`);
+        console.log
+        commandFile.run(client, message, args);
+        console.log
+
+        } catch (err) {
+            console.error(err);
+            console.log
+        }
+    });
 
 
-client.on('message', message => {
 
-const help = require("./commands/Umaru/help.js");
-const invite = require("./commands/Umaru/invite.js");
-const test = require("./commands/Umaru/test.js");
-
-help(message, prefix, client)
-invite(message, prefix, client)
-test(message, prefix, client)
-
-
-
-  });
-
-client.login(token)
+client.login(token);
